a57ff52eb9556bfdaf2de35ae5d5b7066a6ea673
Julian Wergieluk <julian@wergieluk.com>
2019-02-16 19:29:38 +0100

    Add a dummy Agent class

--- rl1.py
+++ rl1.py
@@ -2,23 +2,38 @@ import numpy
 import time
 import subprocess
 import gym
+import random
 
 
 def render(env, cum_reward):
     subprocess.call('clear', shell=False)
     env.render()
-    print(cum_reward)
+    print(f'cum_rew {cum_reward}')
     time.sleep(1.0 / 5.0)
 
 
-def run(env, max_episodes=10):
-    env.reset()
+class Agent1:
+    def __init__(self, env):
+        self.dim_s = env.observation_space.n
+        self.dim_a = env.action_space.n
+
+    def step(self, state, action, reward, next_state, done):
+        pass
+
+    def select_action(self, state):
+        return random.randint(0, self.dim_a-1)
+
+
+def run(env, agent, max_episodes=10):
+    state = env.reset()
     cum_reward = 0
     for _ in range(max_episodes):
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
+        action = agent.select_action(state)
+        next_state, reward, done, _ = env.step(action)
 
+        agent.step(state, action, reward, next_state, done)
         cum_reward += reward
+        state = next_state
         if done:
             return cum_reward
 
@@ -27,5 +42,5 @@ def run(env, max_episodes=10):
 
 if __name__ == '__main__':
     environment = gym.make('Taxi-v2')
-    end_reward = run(environment)
+    end_reward = run(environment, Agent1(environment))
 
