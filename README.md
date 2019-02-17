44319fd1ea60e9088fbb3a6af2ebe35bb0c121c7
Julian Wergieluk <julian@wergieluk.com>
2019-02-17 22:31:53 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Reinforcement learning experiments
+
+* rl01.py -- solution of taxi-v2 using standard temporal difference methods.
+
+
+
+
--- rl01.py
+++ rl01.py
@@ -18,18 +18,18 @@ class Agent1:
     def __init__(self, env):
         self.dim_s = env.observation_space.n
         self.dim_a = env.action_space.n
-        self.Q = numpy.zeros((self.dim_s, self.dim_a))
-        self.alpha = 0.1
-        self.gamma = 0.98
-        self.epsilon = 0.3
-        self.steps_no = 1.0
+        self.Q = numpy.random.randn(self.dim_s, self.dim_a) + 15.0
+        self.alpha = 0.05
+        self.gamma = 0.9
+        self.episodes_no = 1.0
 
     def set_optimal(self):
-        self.epsilon = 0.0
+        self.episodes_no = 20000.0
 
     def step(self, state, action, reward, next_state, done):
-        self.steps_no += 1.0
-        self.sarsa_update(state, action, reward, next_state)
+        if done:
+            self.episodes_no += 1.0
+        self.expected_sarsa_update(state, action, reward, next_state)
 
     def sarsa_update(self, state, action, reward, next_state):
         """ Sarsa on-policy Q-table update rule """
@@ -46,22 +46,34 @@ class Agent1:
     def expected_sarsa_update(self, state, action, reward, next_state):
         """ Expected Sarsa on-policy Q-table update rule """
         next_action = numpy.argmax(self.Q[next_state, :])
-        policy_vector_for_next_state = numpy.repeat(self.epsilon/self.dim_a, self.dim_a)
-        policy_vector_for_next_state[next_action] += 1.0 - self.epsilon
+        policy_vector_for_next_state = numpy.repeat(self.epsilon()/self.dim_a, self.dim_a)
+        policy_vector_for_next_state[next_action] += 1.0 - self.epsilon()
         self.Q[state, action] = self.Q[state, action] * (1.0 - self.alpha) + self.alpha * (
                 reward + self.gamma * numpy.dot(policy_vector_for_next_state, self.Q[next_state, :]))
 
+    def epsilon(self):
+        n = self.episodes_no
+        if n < 2500.0:
+            return 0.1
+#        if n < 5000.0:
+#            return 0.7
+#        if n < 10000.0:
+#            return 0.5
+#        if n < 15000.0:
+#            return 0.2
+#        if n < 19900.0:
+#            return 0.01
+        return 0.0
+
     def select_action(self, state):
-        scale_factor = math.exp(-self.steps_no / 150000.0)
-        if self.steps_no % 5000 == 0:
-            print(self.steps_no, scale_factor)
-        if random.random() <= self.epsilon * scale_factor:
+        if random.random() <= self.epsilon():
             return random.randint(0, self.dim_a-1)
         return numpy.argmax(self.Q[state, :])
 
 
-def train(env, agent, max_episodes=50000):
+def train(env, agent, max_episodes=20000):
     end_rewards = []
+    best_avg_reward = -math.inf
     for i in range(max_episodes):
         state = env.reset()
         cum_reward = 0
@@ -74,9 +86,11 @@ def train(env, agent, max_episodes=50000):
             if done:
                 break
         end_rewards.append(cum_reward)
-        if i % 500 == 0:
-            print(i, cum_reward)
-    return numpy.mean(end_rewards)
+        avg = numpy.mean(end_rewards[-100:])
+        best_avg_reward = avg if avg > best_avg_reward else best_avg_reward
+        if i % 250 == 0:
+            print(i, best_avg_reward, numpy.mean(agent.Q))
+    return best_avg_reward
 
 
 def visualize(env, agent):
@@ -95,10 +109,10 @@ def visualize(env, agent):
 if __name__ == '__main__':
     environment = gym.make('Taxi-v2')
     ag = Agent1(environment)
-    train(environment, ag)
-    ag.set_optimal()
-    max_mean_reward = train(environment, ag, 100)
-    # visualize(environment, ag)
-
+    max_mean_reward = train(environment, ag)
     print('max_mean_reward', max_mean_reward)
+
+    #ag.set_optimal()
+    #max_mean_reward = train(environment, ag, 100)
+    # visualize(environment, ag)
 
